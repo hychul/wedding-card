5535694adba97f81cddfd99d8dbae0db21cd4576
hychul <hychome@gmail.com>
2023-01-08 22:18:42 +0900

    Update bus content

--- src/main/javascript/container/MapContainer.js
+++ src/main/javascript/container/MapContainer.js
@@ -81,9 +81,16 @@ function MapContainer() {
             버스 - 합정역 / 우리은행서교동지정 / 서교동교회 하차
           </div>
           <table></table>
-          <div>파랑(일반/건선) 271A, 921, 602, 603, 604, 761</div>
+          <div>파랑(간선) 271A, 921, 602, 603, 604, 761</div>
           <div>초록(지선) 마포08, 마포09, 마포15, 마포16</div>
-          <div>5712, 5714, 6712, 6716, 7612, 7734, 7739</div>
+          <div style={{ marginLeft: "4.5rem" }}>
+            5712, 5714, 6712, 6716, 7612, 7734, 7739
+          </div>
+          <div>빨강(광역) 1000, 1100, 1101, 1200, 1300, 1301, 1302,</div>
+          <div style={{ marginLeft: "4.5rem" }}>1601, 3100</div>
+          <div style={{ marginLeft: "4.5rem" }}>
+            삼화고속 (인천 → 서울) 합정 전철역
+          </div>
         </div>
         <div
           style={{
@@ -93,9 +100,26 @@ function MapContainer() {
             padding: "0px 30px",
           }}
         >
-          <div style={{ fontWeight: "bold" }}>제1주차장</div>
-          <div style={{ fontWeight: "bold" }}>제2주차장</div>
-          <div style={{ fontWeight: "bold" }}>제3주차장</div>
+          <div
+            style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+          >
+            <div style={{ fontWeight: "bold" }}>제1주차장</div>
+            <div>본건물</div>
+          </div>
+          <div
+            style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+          >
+            <div style={{ fontWeight: "bold" }}>제2주차장</div>
+            <div>H스퀘어</div>
+          </div>
+          <div
+            style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+          >
+            <div style={{ fontWeight: "bold" }}>
+              제3주차장 : 서교빌딩 주차장
+            </div>
+            <div>서교빌딩 주차장</div>
+          </div>
         </div>
       </div>
     </div>
