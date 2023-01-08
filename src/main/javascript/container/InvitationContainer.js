eb75c990042a758d3cbcaeb09a9f4b0da4e20c14
hychul <hychome@gmail.com>
2023-01-08 18:05:30 +0900

    Add invitation contents

--- src/main/javascript/container/InvitationContainer.js
+++ src/main/javascript/container/InvitationContainer.js
@@ -2,11 +2,46 @@ function InvitationContainer() {
   return (
     <div
       style={{
-        backgroundColor: "paleGreen",
-        height: "500px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "30px",
+        backgroundColor: "white",
+        width: "100%",
+        padding: "20px",
+        justifyContent: "space-between",
       }}
     >
-      invitation
+      <div
+        style={{
+          width: "100%",
+          textAlign: "center",
+          color: "pink",
+          fontSize: "1.25em",
+        }}
+      >
+        Invitation
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          width: "100%",
+          textAlign: "center",
+          color: "black",
+        }}
+      >
+        <div>2020년 늦봄, 봄비가 내리던 날 저희는 처음 만났습니다.</div>
+        <br />
+        <div>서로 마주앉아 시간가는 줄 모르고 대화를 하다보니,</div>
+        <div>유진이와 모든 일상을 함께 하고싶은 생각이 들었습니다.</div>
+        <br />
+        <div>집으로 돌아갈 떄쯤 형철이가 제 손을 꼭 잡았습니다.</div>
+        <div>저는 이 손을 놓지 않을 것 같다는 생각이 들었습니다.</div>
+        <br />
+        <div>2023년 초봄, 두 손을 꼭 잡고 평생을 함께 하려합니다.</div>
+        <div>오셔서 저희의 출발을 축하해주세요.</div>
+      </div>
     </div>
   );
 }
--- src/main/javascript/container/MainContainer.js
+++ src/main/javascript/container/MainContainer.js
@@ -2,11 +2,36 @@ function MainContainer() {
   return (
     <div
       style={{
+        display: "flex",
+        flexDirection: "column",
         backgroundColor: "pink",
+        width: "100%",
         height: "500px",
+        padding: "20px",
+        justifyContent: "space-between",
       }}
     >
-      Main
+      <div
+        style={{
+          width: "100%",
+          textAlign: "center",
+          color: "white",
+          fontSize: "1.25em",
+        }}
+      >
+        <div>조형철</div>
+        <div>-</div>
+        <div>채유진</div>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        2023년 3월 18일 토요일 PM 03시 웨딩 시그니처
+      </div>
     </div>
   );
 }
