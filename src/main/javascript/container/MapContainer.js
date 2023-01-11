eda5d7302da4a668c508ec201368cd5f2e3b00e4
hychul <hychome@gmail.com>
2023-01-11 22:03:40 +0900

    Set map info content to center

--- src/main/javascript/container/MapContainer.js
+++ src/main/javascript/container/MapContainer.js
@@ -84,72 +84,79 @@ function MapContainer() {
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "20px",
-          width: "100%",
+          flexDirection: "row",
+          justifyContent: "center",
         }}
       >
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            gap: "5px",
-            width: "90%",
-          }}
-        >
-          <div style={{ fontWeight: "bold" }}>지하철</div>
-          <div>2호선, 6호선 합정역 하자 - 2번 추구 도보 4분</div>
-          <div>홍대입구, 공항철도, 경의선 1번출구 도보 11분</div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "5px",
-            width: "90%",
-          }}
-        >
-          <div style={{ fontWeight: "bold" }}>
-            버스 - 합정역 / 우리은행 서교동지점 / 서교동교회 하차
-          </div>
-          <table></table>
-          <div>파랑(간선) 271A, 921, 602, 603, 604, 761</div>
-          <div>초록(지선) 마포08, 마포09, 마포15, 마포16</div>
-          <div style={{ marginLeft: "4.5em" }}>
-            5712, 5714, 6712, 6716, 7612, 7734, 7739
-          </div>
-          <div>빨강(광역) 1000, 1100, 1101, 1200, 1300, 1301, 1302,</div>
-          <div style={{ marginLeft: "4.5em" }}>1601, 3100</div>
-          <div style={{ marginLeft: "4.5em" }}>
-            삼화고속 (인천 → 서울) 합정 전철역
-          </div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "5px",
-            width: "90%",
+            alignItems: "center",
+            gap: "20px",
           }}
         >
           <div
-            style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "5px",
+              width: "90%",
+            }}
           >
-            <div style={{ fontWeight: "bold" }}>제1주차장</div>
-            <div>본건물</div>
+            <div style={{ fontWeight: "bold" }}>지하철</div>
+            <div>2호선, 6호선 합정역 하자 - 2번 추구 도보 4분</div>
+            <div>홍대입구, 공항철도, 경의선 1번출구 도보 11분</div>
           </div>
           <div
-            style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "5px",
+              width: "90%",
+            }}
           >
-            <div style={{ fontWeight: "bold" }}>제2주차장</div>
-            <div>H스퀘어</div>
+            <div style={{ fontWeight: "bold" }}>
+              버스 - 합정역 / 우리은행 서교동지점 / 서교동교회 하차
+            </div>
+            <table></table>
+            <div>파랑(간선) 271A, 921, 602, 603, 604, 761</div>
+            <div>초록(지선) 마포08, 마포09, 마포15, 마포16</div>
+            <div style={{ marginLeft: "4.5em" }}>
+              5712, 5714, 6712, 6716, 7612, 7734, 7739
+            </div>
+            <div>빨강(광역) 1000, 1100, 1101, 1200, 1300, 1301, 1302,</div>
+            <div style={{ marginLeft: "4.5em" }}>1601, 3100</div>
+            <div style={{ marginLeft: "4.5em" }}>
+              삼화고속 (인천 → 서울) 합정 전철역
+            </div>
           </div>
           <div
-            style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "5px",
+              width: "90%",
+            }}
           >
-            <div style={{ fontWeight: "bold" }}>제3주차장</div>
-            <div>서교빌딩 주차장</div>
+            <div
+              style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+            >
+              <div style={{ fontWeight: "bold" }}>제1주차장</div>
+              <div>본건물</div>
+            </div>
+            <div
+              style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+            >
+              <div style={{ fontWeight: "bold" }}>제2주차장</div>
+              <div>H스퀘어</div>
+            </div>
+            <div
+              style={{ display: "flex", flexDirection: "row", gap: "0.25rem" }}
+            >
+              <div style={{ fontWeight: "bold" }}>제3주차장</div>
+              <div>서교빌딩 주차장</div>
+            </div>
           </div>
         </div>
       </div>
