a642430c1a969929d702fbd6238d2a8776e99f44
hychul <hychome@gmail.com>
2023-01-11 22:05:48 +0900

    Change account container style

--- src/main/javascript/container/AccountContainer.js
+++ src/main/javascript/container/AccountContainer.js
@@ -2,12 +2,42 @@ function AccountContainer() {
   return (
     <div
       style={{
-        backgroundColor: "white",
-        height: "500px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: "30px",
         padding: "50px 0px",
+        width: "100%",
+        backgroundColor: "white",
       }}
     >
-      Account
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            color: "pink",
+            fontSize: "1.25rem",
+          }}
+        >
+          마음을 전하는 곳
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
+        >
+          <div>신랑측 계좌번호</div>
+          <div>신부측 계좌번호</div>
+        </div>
+      </div>
     </div>
   );
 }
