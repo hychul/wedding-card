49a0444560806d5a1988503a7cf6b87f5bdee447
hychul <hychome@gmail.com>
2023-01-15 23:55:44 +0900

    Add main image to main container

--- src/main/javascript/container/MainContainer.js
+++ src/main/javascript/container/MainContainer.js
@@ -1,33 +1,47 @@
+import imgMain from "main/resource/image/main.jpeg";
+
 function MainContainer() {
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        backgroundColor: "#fab9c3",
-        padding: "50px 0px",
-        width: "100%",
-        height: "500px",
-        justifyContent: "space-between",
+        justifyContent: "end",
+        alignItems: "center",
+        gap: "15px",
+        padding: "15px 0px",
+        width: "100vw",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
       }}
     >
       <div
         style={{
-          width: "100%",
+          borderRadius: "5px",
+          padding: "5px",
+          backgroundColor: "white",
+          fontSize: "1.2rem",
           textAlign: "center",
-          color: "white",
-          fontSize: "1.25rem",
+          color: "black",
         }}
       >
-        <div>조형철</div>
-        <div>-</div>
-        <div>채유진</div>
+        조형철 & 채유진
       </div>
+      <img
+        style={{
+          width: "100vw",
+          height: "150vw",
+          objectFit: "contain",
+        }}
+        src={imgMain}
+      ></img>
       <div
         style={{
-          width: "100%",
+          borderRadius: "5px",
+          backgroundColor: "white",
+          fontSize: "0.9rem",
           textAlign: "center",
-          color: "white",
+          color: "black",
         }}
       >
         2023년 3월 18일 토요일 PM 03시 웨딩 시그니처
