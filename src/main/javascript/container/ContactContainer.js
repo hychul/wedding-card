585f713deb841e3de24c4659478f363c6f793405
hychul <hychome@gmail.com>
2023-01-15 17:40:52 +0900

    Set icon for call and sms

--- src/main/javascript/container/ContactContainer.js
+++ src/main/javascript/container/ContactContainer.js
@@ -1,3 +1,6 @@
+import call from "main/resource/call.svg";
+import sms from "main/resource/sms.svg";
+
 function ContactContainer() {
   return (
     <div
@@ -15,6 +18,7 @@ function ContactContainer() {
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
+          alignItems: "center",
           gap: "0.3rem",
           width: "100%",
         }}
@@ -36,14 +40,37 @@ function ContactContainer() {
         </div>
         <div style={{ fontSize: "1.1rem" }}>조형철</div>
         <div style={{ padding: "0em 0.5rem" }}>···</div>
-        <a href="tel:010-2775-7106">전화</a>
-        <a href="sms:010-2775-7106">문자</a>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <a href="tel:010-2775-7106">
+            <img
+              style={{ width: "0.8rem", height: "0.8rem" }}
+              src={call}
+              alt="전화"
+            />
+          </a>
+          <a href="sms:010-2775-7106">
+            <img
+              style={{ width: "0.8rem", height: "0.8rem" }}
+              src={sms}
+              alt="문자"
+            />
+          </a>
+        </div>
       </div>
       <div
         style={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
+          alignItems: "center",
           gap: "0.3rem",
           width: "100%",
         }}
@@ -65,8 +92,30 @@ function ContactContainer() {
         </div>
         <div style={{ fontSize: "1.1rem" }}>채유진</div>
         <div style={{ padding: "0em 0.5rem" }}>···</div>
-        <a href="tel:010-5422-1081">전화</a>
-        <a href="sms:010-5422-1081">문자</a>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <a href="tel:010-5422-1081">
+            <img
+              style={{ width: "0.8rem", height: "0.8rem" }}
+              src={call}
+              alt="전화"
+            />
+          </a>
+          <a href="sms:010-5422-1081">
+            <img
+              style={{ width: "0.8rem", height: "0.8rem" }}
+              src={sms}
+              alt="문자"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
